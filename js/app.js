eb16f74feb9ef3de392b51f88f81e116f98a4cc9
pdxoliver <rms.moliver@gmail.com>
2017-10-06 10:52:30 -0700

    Added animations

--- js/app.js
+++ js/app.js
@@ -1,42 +1,10 @@
-const menu = document.getElementById('nav-reveal');
-const homeScreen = document.getElementById('header');
-const hideDivs = document.getElementsByClassName('hide-div');
-const navBtns = document.getElementsByClassName('btn');
-const about = document.getElementById('about');
-const portfolio = document.getElementById('projects');
-const projects = document.getElementById('portfolio');
-const home = document.getElementsByClassName('home-btn');
+const nav = document.getElementById('nav');
+const menuBtn = document.getElementById('menu');
 
-
-if($(window).width() > 1024) {
-	$(hideDivs).removeClass('hide');
-
-	$('.menu').click(function() {
-		$(homeScreen).removeClass('show');
-		$(homeScreen).addClass('hide');
-		$(menu).removeClass('hide');
-		$(menu).addClass('show');
-	});
-
-	$(navBtns).click(function() {
-		let btn = $(this).text();
-		$(menu).addClass('hide');
-		$(menu).removeClass('show');
-		if(btn === 'About') {
-			$(about).removeClass('hide');
-			$(about).addClass('show');	
-		} else if(btn === 'Projects') {
-			$(portfolio).removeClass('hide');
-			$(projects).removeClass('hide');
-			$(portfolio).addClass('show');
-			$(projects).addClass('show');
-
-		}
-	});
-
-	$(home).click(function() {
-		let currentDiv = (this).parentNode;
-		$(currentDiv).removeClass('show');
-		$(currentDiv).addClass('hide')
-	});
-}
+$(menuBtn).click(function() {
+	$(this).css("animation", "move 1s ease-out");
+	$(this).addClass('hide');
+	$(nav).css("animation", "move 1s ease-out");
+	$(nav).removeClass('hide');
+	$(nav).addClass('show');
+});
