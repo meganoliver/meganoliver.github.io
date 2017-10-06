12cddef5394ff6e1628bda5eb4f464c0aa811f3b
pdxoliver <rms.moliver@gmail.com>
2017-10-06 13:49:16 -0700

    add scrolling function in js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const nav = document.getElementById('nav');
 const menuBtn = document.getElementById('menu');
+const navBtns = document.getElementsByClassName('btn');
 
 $(menuBtn).click(function() {
 	$(this).css("animation", "move 1s ease-out");
@@ -7,4 +8,16 @@ $(menuBtn).click(function() {
 	$(nav).css("animation", "move 1s ease-out");
 	$(nav).removeClass('hide');
 	$(nav).addClass('show');
+});
+
+$("a[href='#header']").click(function() {
+	$("html, body").animate({ scrollTop: 0 }, "slow");
+	return false;
+});
+
+$(navBtns).click(function() {
+	let link = $(this).text().toLowerCase();
+	let div = document.getElementById(link);
+	$('html, body').animate({
+		scrollTop: $(div).offset().top}, 'slow');
 });
